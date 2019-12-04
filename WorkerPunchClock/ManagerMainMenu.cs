2b59c7f1d334d8ca0cfbecdb547bb56d0d2a33cf
Anastasia710 <31499133+Anastasia710@users.noreply.github.com>
2019-12-03 19:55:07 -0700

    Added the ablility to use Manager Page

--- WorkerPunchClock/ManagerMainMenu.cs
+++ WorkerPunchClock/ManagerMainMenu.cs
@@ -15,6 +15,142 @@ namespace WorkerPunchClock
         public ManagerMainMenu()
         {
             InitializeComponent();
+            ManagerPageTimer.Start();
         }
+
+        private void ManagerMainMenuClosing(object sender, FormClosingEventArgs e)
+        {
+            var staffLogin = new StaffLogin();
+            Hide();
+            staffLogin.Show();
+        }
+
+        private void ManagerPageTimer_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            this.TimeLabel.Text = dateTime.ToString();
+        }
+
+        private void WorkSchedulePictureBox_Click(object sender, EventArgs e)
+        {
+            new WorkSchedule().Show();
+        }
+
+        private void WorkScheduleMouseHover(object sender, EventArgs e)
+        {
+            WorkScheduleLabel.ForeColor = Color.Red;
+        }
+
+        private void WorkScheduleMouseLeave(object sender, EventArgs e)
+        {
+            WorkScheduleLabel.ForeColor = Color.Black;
+        }
+
+        private void ClockInOutPictureBox_Click(object sender, EventArgs e)
+        {
+            new ClockInOut().Show();
+        }
+
+        private void ClockInOutMouseHover(object sender, EventArgs e)
+        {
+            ClockInOutLabel.ForeColor = Color.Red;
+        }
+
+        private void ClockInOutMouseLeave(object sender, EventArgs e)
+        {
+            ClockInOutLabel.ForeColor = Color.Black;
+        }
+
+        private void RequestTimeOffPictureBox_Click(object sender, EventArgs e)
+        {
+            new RequestTimeOff().Show();
+        }
+
+        private void RequestTimeOffMouseHover(object sender, EventArgs e)
+        {
+            RequestTimeOffLabel.ForeColor = Color.Red;
+        }
+
+        private void RequestTimeOffMouseLeave(object sender, EventArgs e)
+        {
+            RequestTimeOffLabel.ForeColor = Color.Black;
+        }
+
+        private void PayStubPictureBox_Click(object sender, EventArgs e)
+        {
+            new PayStub().Show();
+        }
+
+        private void PayStubMouseHover(object sender, EventArgs e)
+        {
+            PayStubLabel.ForeColor = Color.Red;
+        }
+
+        private void PayStubMouseLeave(object sender, EventArgs e)
+        {
+            PayStubLabel.ForeColor = Color.Black;
+        }
+
+        private void CreateEditSchedulePictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateEditScheduleMouseHover(object sender, EventArgs e)
+        {
+            CreateEditScheduleLabel.ForeColor = Color.Red;
+        }
+
+        private void CreateEditScheduleMouseLeave(object sender, EventArgs e)
+        {
+            CreateEditScheduleLabel.ForeColor = Color.Black;
+        }
+
+        private void CreateEditEmployeePictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateEditEmployeeMouseHover(object sender, EventArgs e)
+        {
+            CreateEditEmployeeLabel.ForeColor = Color.Red;
+        }
+
+        private void CreateEditEmployeeMouseLeave(object sender, EventArgs e)
+        {
+            CreateEditEmployeeLabel.ForeColor = Color.Black;
+        }
+
+        private void ApprovePayStubsPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ApprovePayStubsMouseHover(object sender, EventArgs e)
+        {
+            ApprovePayStubsLabel.ForeColor = Color.Red;
+        }
+
+        private void ApprovePayStubsMouseLeave(object sender, EventArgs e)
+        {
+            ApprovePayStubsLabel.ForeColor = Color.Black;
+        }
+
+        private void ApproveDenyTimeOffPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ApproveDenyTimeOffMouseHover(object sender, EventArgs e)
+        {
+            ApproveDenyTimeOffLabel.ForeColor = Color.Red;
+        }
+
+        private void ApproveDenyTimeOffMouseLeave(object sender, EventArgs e)
+        {
+            ApproveDenyTimeOffLabel.ForeColor = Color.Black;
+        }
+
+        
     }
 }
