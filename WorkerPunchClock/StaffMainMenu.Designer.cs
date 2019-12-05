8a036c1ab329e648dfb650faf23d1709a42991bb
Cole Tucker <c.tucker980@mybvc.ca>
2019-12-04 20:56:31 -0700

    Deleted TopBar

--- WorkerPunchClock/StaffMainMenu.Designer.cs
+++ WorkerPunchClock/StaffMainMenu.Designer.cs
@@ -29,11 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.TimeLabel = new System.Windows.Forms.Label();
-            this.splitter1 = new System.Windows.Forms.Splitter();
-            this.StaffNameLabel = new System.Windows.Forms.Label();
-            this.NameSplitter = new System.Windows.Forms.Splitter();
             this.WorkScheduleLabel = new System.Windows.Forms.Label();
             this.TimeTimerTicker = new System.Windows.Forms.Timer(this.components);
             this.ClockInOutLabel = new System.Windows.Forms.Label();
@@ -43,67 +38,11 @@
             this.RequestTimeOffPictureBox = new System.Windows.Forms.PictureBox();
             this.ClockInOutPictureBox = new System.Windows.Forms.PictureBox();
             this.WorkSchedulePictureBox = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PayStubPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.RequestTimeOffPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ClockInOutPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.WorkSchedulePictureBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.Silver;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.TimeLabel);
-            this.panel1.Controls.Add(this.splitter1);
-            this.panel1.Controls.Add(this.StaffNameLabel);
-            this.panel1.Controls.Add(this.NameSplitter);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 34);
-            this.panel1.TabIndex = 1;
-            // 
-            // TimeLabel
-            // 
-            this.TimeLabel.AutoSize = true;
-            this.TimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TimeLabel.Location = new System.Drawing.Point(143, 8);
-            this.TimeLabel.Name = "TimeLabel";
-            this.TimeLabel.Size = new System.Drawing.Size(144, 13);
-            this.TimeLabel.TabIndex = 3;
-            this.TimeLabel.Text = "00/00/0000 0:00:00 AM";
-            // 
-            // splitter1
-            // 
-            this.splitter1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.splitter1.Enabled = false;
-            this.splitter1.Location = new System.Drawing.Point(137, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(162, 32);
-            this.splitter1.TabIndex = 2;
-            this.splitter1.TabStop = false;
-            // 
-            // StaffNameLabel
-            // 
-            this.StaffNameLabel.AutoSize = true;
-            this.StaffNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StaffNameLabel.Location = new System.Drawing.Point(11, 8);
-            this.StaffNameLabel.Name = "StaffNameLabel";
-            this.StaffNameLabel.Size = new System.Drawing.Size(116, 13);
-            this.StaffNameLabel.TabIndex = 1;
-            this.StaffNameLabel.Text = "Name: Cole Tucker";
-            // 
-            // NameSplitter
-            // 
-            this.NameSplitter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.NameSplitter.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.NameSplitter.Enabled = false;
-            this.NameSplitter.Location = new System.Drawing.Point(0, 0);
-            this.NameSplitter.Name = "NameSplitter";
-            this.NameSplitter.Size = new System.Drawing.Size(137, 32);
-            this.NameSplitter.TabIndex = 0;
-            this.NameSplitter.TabStop = false;
             // 
             // WorkScheduleLabel
             // 
@@ -221,15 +160,12 @@
             this.Controls.Add(this.ClockInOutLabel);
             this.Controls.Add(this.ClockInOutPictureBox);
             this.Controls.Add(this.WorkScheduleLabel);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.WorkSchedulePictureBox);
             this.Name = "StaffMainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Staff Main Menu";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StaffMainMenuClosing);
             this.Load += new System.EventHandler(this.StaffMainMenu_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PayStubPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.RequestTimeOffPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ClockInOutPictureBox)).EndInit();
@@ -242,12 +178,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox WorkSchedulePictureBox;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label WorkScheduleLabel;
-        private System.Windows.Forms.Splitter NameSplitter;
-        private System.Windows.Forms.Label StaffNameLabel;
-        private System.Windows.Forms.Splitter splitter1;
-        private System.Windows.Forms.Label TimeLabel;
         private System.Windows.Forms.Timer TimeTimerTicker;
         private System.Windows.Forms.PictureBox ClockInOutPictureBox;
         private System.Windows.Forms.Label ClockInOutLabel;
