e871b7c5c510267f6ba80ba7cfb47ab5eb71fa98
Anastasia710 <31499133+Anastasia710@users.noreply.github.com>
2019-12-14 13:29:28 -0700

    Adding designs

--- WorkerPunchClock/ApproveDenyTimeOff.cs
+++ WorkerPunchClock/ApproveDenyTimeOff.cs
@@ -16,5 +16,26 @@ namespace WorkerPunchClock
         {
             InitializeComponent();
         }
+
+        private void EmployeeNameComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /// TO DO
+            /// This should display the employee names
+            /// depending on the employee name it should
+            /// change what gets displayed in the timeoff panel
+        }
+
+        private void ApproveTimeOffButton_Click(object sender, EventArgs e)
+        {
+            ///To do 
+            ///once button clicks the dates shown should be blocked off 
+            ///on schedule
+        }
+
+        private void DenyTimeOffButton_Click(object sender, EventArgs e)
+        {
+            ///to do
+            /// not sure???
+        }
     }
 }
