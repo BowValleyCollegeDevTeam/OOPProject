b8506a8e088b9fc754bafb936a0048a86b4eaf53
allenjdonohue <45175528+allenjdonohue@users.noreply.github.com>
2019-12-16 03:10:03 -0700

    added check status function

--- WorkerPunchClock/PayStub.cs
+++ WorkerPunchClock/PayStub.cs
@@ -15,6 +15,38 @@ namespace WorkerPunchClock
     public partial class PayStub : Form
     {
         public string str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ajdon\Documents\GitHub\OOPProject\WorkerPunchClock\Workers.mdf;Integrated Security = True";
+
+        public void CheckStatus()
+        {
+            using (StaffLogin login = new StaffLogin())
+            using (SqlConnection myConnection = new SqlConnection(str))
+            using (SqlDataAdapter employeePin = new SqlDataAdapter($"SELECT * FROM Employees WHERE PIN = {login.pin}", myConnection))
+            {
+                DataTable userPin = new DataTable();
+
+                myConnection.Open();
+                employeePin.Fill(userPin);
+                myConnection.Close();
+                for (int row = 0; row < userPin.Rows.Count; row++)
+                {
+                    string FirstName = (string)userPin.Rows[row]["FName"];
+                    string LastName = (string)userPin.Rows[row]["LName"];
+                    bool Status = (bool)userPin.Rows[row]["Status"];
+
+                    this.topInfoBar1.StaffNameLabel.Text = "Name: " + FirstName + " " + LastName;
+                    if (Status == false)
+                    {
+                        this.topInfoBar1.StatusLabel.Text = "Status: " + "Clocked Out";
+                    }
+                    else if (Status == true)
+                    {
+                        this.topInfoBar1.StatusLabel.Text = "Status: " + "Clocked In";
+                    }
+
+                }
+            }
+        }
+
         public PayStub()
         {
             InitializeComponent();
@@ -32,6 +64,7 @@ namespace WorkerPunchClock
 
         private void PayStub_Load(object sender, EventArgs e)
         {
+            CheckStatus();
             using (SqlConnection myConnection = new SqlConnection(str))
             {
                 int employeeID = 0;
