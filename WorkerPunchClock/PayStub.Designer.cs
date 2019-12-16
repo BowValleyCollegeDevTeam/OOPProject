3b991318c0aa8c4c95f728019fbc41365786e6d4
allenjdonohue <45175528+allenjdonohue@users.noreply.github.com>
2019-12-16 01:04:28 -0700

    pay stub done

--- WorkerPunchClock/PayStub.Designer.cs
+++ WorkerPunchClock/PayStub.Designer.cs
@@ -28,116 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.TimeLabel = new System.Windows.Forms.Label();
-            this.splitter1 = new System.Windows.Forms.Splitter();
-            this.StaffNameLabel = new System.Windows.Forms.Label();
-            this.NameSplitter = new System.Windows.Forms.Splitter();
-            this.payStubTextBox = new System.Windows.Forms.RichTextBox();
-            this.payStubComboBox = new System.Windows.Forms.ComboBox();
-            this.panel1.SuspendLayout();
+            this.payStubListBox = new System.Windows.Forms.ListBox();
+            this.topInfoBar1 = new WorkerPunchClock.TopInfoBar();
+            this.payStubLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // panel1
+            // payStubListBox
             // 
-            this.panel1.BackColor = System.Drawing.Color.Silver;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.TimeLabel);
-            this.panel1.Controls.Add(this.splitter1);
-            this.panel1.Controls.Add(this.StaffNameLabel);
-            this.panel1.Controls.Add(this.NameSplitter);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 34);
-            this.panel1.TabIndex = 3;
+            this.payStubListBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.payStubListBox.FormattingEnabled = true;
+            this.payStubListBox.Location = new System.Drawing.Point(0, 95);
+            this.payStubListBox.Name = "payStubListBox";
+            this.payStubListBox.Size = new System.Drawing.Size(800, 355);
+            this.payStubListBox.TabIndex = 7;
             // 
-            // TimeLabel
+            // topInfoBar1
             // 
-            this.TimeLabel.AutoSize = true;
-            this.TimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TimeLabel.Location = new System.Drawing.Point(143, 8);
-            this.TimeLabel.Name = "TimeLabel";
-            this.TimeLabel.Size = new System.Drawing.Size(144, 13);
-            this.TimeLabel.TabIndex = 3;
-            this.TimeLabel.Text = "00/00/0000 0:00:00 AM";
+            this.topInfoBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topInfoBar1.Location = new System.Drawing.Point(0, 0);
+            this.topInfoBar1.Name = "topInfoBar1";
+            this.topInfoBar1.Size = new System.Drawing.Size(800, 35);
+            this.topInfoBar1.TabIndex = 6;
             // 
-            // splitter1
+            // payStubLabel
             // 
-            this.splitter1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.splitter1.Enabled = false;
-            this.splitter1.Location = new System.Drawing.Point(137, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(162, 32);
-            this.splitter1.TabIndex = 2;
-            this.splitter1.TabStop = false;
-            // 
-            // StaffNameLabel
-            // 
-            this.StaffNameLabel.AutoSize = true;
-            this.StaffNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StaffNameLabel.Location = new System.Drawing.Point(11, 8);
-            this.StaffNameLabel.Name = "StaffNameLabel";
-            this.StaffNameLabel.Size = new System.Drawing.Size(116, 13);
-            this.StaffNameLabel.TabIndex = 1;
-            this.StaffNameLabel.Text = "Name: Cole Tucker";
-            // 
-            // NameSplitter
-            // 
-            this.NameSplitter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.NameSplitter.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.NameSplitter.Enabled = false;
-            this.NameSplitter.Location = new System.Drawing.Point(0, 0);
-            this.NameSplitter.Name = "NameSplitter";
-            this.NameSplitter.Size = new System.Drawing.Size(137, 32);
-            this.NameSplitter.TabIndex = 0;
-            this.NameSplitter.TabStop = false;
-            // 
-            // payStubTextBox
-            // 
-            this.payStubTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.payStubTextBox.Location = new System.Drawing.Point(0, 111);
-            this.payStubTextBox.Name = "payStubTextBox";
-            this.payStubTextBox.Size = new System.Drawing.Size(800, 339);
-            this.payStubTextBox.TabIndex = 4;
-            this.payStubTextBox.Text = "";
-            // 
-            // payStubComboBox
-            // 
-            this.payStubComboBox.FormattingEnabled = true;
-            this.payStubComboBox.Items.AddRange(new object[] {
-            "Select Pay Period"});
-            this.payStubComboBox.Location = new System.Drawing.Point(1, 40);
-            this.payStubComboBox.Name = "payStubComboBox";
-            this.payStubComboBox.Size = new System.Drawing.Size(137, 21);
-            this.payStubComboBox.TabIndex = 5;
-            this.payStubComboBox.SelectedIndexChanged += new System.EventHandler(this.payStubComboBox_SelectedIndexChanged);
+            this.payStubLabel.AutoSize = true;
+            this.payStubLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payStubLabel.Location = new System.Drawing.Point(-5, 55);
+            this.payStubLabel.Name = "payStubLabel";
+            this.payStubLabel.Size = new System.Drawing.Size(175, 25);
+            this.payStubLabel.TabIndex = 8;
+            this.payStubLabel.Text = "Paystub History: ";
             // 
             // PayStub
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.payStubComboBox);
-            this.Controls.Add(this.payStubTextBox);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.payStubLabel);
+            this.Controls.Add(this.payStubListBox);
+            this.Controls.Add(this.topInfoBar1);
             this.Name = "PayStub";
             this.Text = "PayStub";
             this.Load += new System.EventHandler(this.PayStub_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label TimeLabel;
-        private System.Windows.Forms.Splitter splitter1;
-        private System.Windows.Forms.Label StaffNameLabel;
-        private System.Windows.Forms.Splitter NameSplitter;
-        private System.Windows.Forms.RichTextBox payStubTextBox;
-        private System.Windows.Forms.ComboBox payStubComboBox;
+        private TopInfoBar topInfoBar1;
+        private System.Windows.Forms.ListBox payStubListBox;
+        private System.Windows.Forms.Label payStubLabel;
     }
 }
