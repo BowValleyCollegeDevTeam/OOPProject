a30582ea00e49a1ccacbef1ad32501af654766db
Cole Tucker <c.tucker980@mybvc.ca>
2019-12-15 21:27:24 -0700

    Updates To ClockInOut.cs

--- WorkerPunchClock/ClockInOut.Designer.cs
+++ WorkerPunchClock/ClockInOut.Designer.cs
@@ -28,25 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            //this.topMenuBar1 = new WorkerPunchClock.TopMenuBar();
             this.ClockInButton = new System.Windows.Forms.Button();
+            this.topInfoBar1 = new WorkerPunchClock.TopInfoBar();
+            this.ClockInOutPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // topMenuBar1
-            // 
-            //this.topMenuBar1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            //this.topMenuBar1.Dock = System.Windows.Forms.DockStyle.Top;
-            //this.topMenuBar1.Location = new System.Drawing.Point(0, 0);
-            //this.topMenuBar1.Name = "topMenuBar1";
-            //this.topMenuBar1.Size = new System.Drawing.Size(800, 35);
-            //this.topMenuBar1.TabIndex = 0;
             // 
             // ClockInButton
             // 
             this.ClockInButton.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClockInButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.ClockInButton.FlatAppearance.BorderSize = 0;
-            this.ClockInButton.Location = new System.Drawing.Point(338, 41);
+            this.ClockInButton.Location = new System.Drawing.Point(298, 41);
             this.ClockInButton.Name = "ClockInButton";
             this.ClockInButton.Size = new System.Drawing.Size(106, 37);
             this.ClockInButton.TabIndex = 1;
@@ -54,15 +46,33 @@
             this.ClockInButton.UseVisualStyleBackColor = false;
             this.ClockInButton.Click += new System.EventHandler(this.ClockInButton_Click);
             // 
+            // topInfoBar1
+            // 
+            this.topInfoBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topInfoBar1.Location = new System.Drawing.Point(0, 0);
+            this.topInfoBar1.Name = "topInfoBar1";
+            this.topInfoBar1.Size = new System.Drawing.Size(416, 35);
+            this.topInfoBar1.TabIndex = 2;
+            // 
+            // ClockInOutPanel
+            // 
+            this.ClockInOutPanel.AutoScroll = true;
+            this.ClockInOutPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ClockInOutPanel.Location = new System.Drawing.Point(0, 35);
+            this.ClockInOutPanel.Name = "ClockInOutPanel";
+            this.ClockInOutPanel.Size = new System.Drawing.Size(292, 415);
+            this.ClockInOutPanel.TabIndex = 3;
+            // 
             // ClockInOut
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(416, 450);
+            this.Controls.Add(this.ClockInOutPanel);
+            this.Controls.Add(this.topInfoBar1);
             this.Controls.Add(this.ClockInButton);
-            //this.Controls.Add(this.topMenuBar1);
             this.Name = "ClockInOut";
-            this.Text = "ClockInOut";
+            this.Text = "pane";
             this.ResumeLayout(false);
 
         }
@@ -71,5 +81,7 @@
 
         //private TopMenuBar topMenuBar1;
         private System.Windows.Forms.Button ClockInButton;
+        private TopInfoBar topInfoBar1;
+        private System.Windows.Forms.Panel ClockInOutPanel;
     }
 }
